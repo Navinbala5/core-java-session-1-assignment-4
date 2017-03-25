e9625b59af7b79c38ac3c3a5e6e4b53cb7695e44
Navinbala5 <bnavin28@gmail.com>
2017-03-25 08:59:09 +0530

    Add files via upload

---
+++ FourthAssignment.java
@@ -0,0 +1,19 @@
+
+public class FourthAssignment {
+
+	public static void main(String[] args) {
+		int number=65;
+		char ascii=(char)number;
+	
+		for(number=65;number<=90;number++)
+			
+		{
+			
+			System.out.println("the  ascii value of " +number+" - " +ascii);
+			ascii++;
+	}
+
+}
+	}
+	
+
